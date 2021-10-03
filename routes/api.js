76cc0d4d78fa6d260ed7b8cb81fc33d5ffb3c3d5
Spardutti <luisdamian.sp@gmail.com>
2021-10-03 18:58:49 -0300

    .

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,6 @@ var express = require("express");
 var router = express.Router();
 const recipeController = require("../Controllers/recipeController");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("culon");
-});
-
 /* CREATE NEW RECIPE */
 router.post("/recipe", recipeController.newRecipe);
 
