9fdb6654bd88f0fbec0b670a0f9e0109fb8b299e
Spardutti <luisdamian.sp@gmail.com>
2021-10-04 16:55:40 -0300

    culon vago

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const Recipe = require("../models/Recipe");
 const Ingredient = require("../models/Ingredient");
 const { body, validationResult } = require("express-validator");
-const { isHttpError } = require("http-errors");
 
 // CUANDO ACTUALIZAS UN ARRAY DE UN OBJECTO DE UNA BASE DE DATOS TIENES QUE PONER    user.markModified("fridge")
 // EJEMPLO PARA CUANDO ACTUALICES EL FRIDGE. USER SERIA EL NOMBRE QUE LE DAS AL USER CUANDO LO BUSQUES EN LA DB.
@@ -11,19 +10,19 @@ const { isHttpError } = require("http-errors");
 /* CREATE NEW USER */
 exports.newUser = async (req, res, next) => {
   try {
-    const { name } = req.body;
+    const { name } = req.body; //name ???
 
     const user = new User({
-      username,
+      username, // de donde sale username ??
     });
 
     //await user.save();
+
+    //no retorna nada..
   } catch (err) {
     res.json(next(err));
   }
 };
-// WITH USERNAME ONLY
-// POST
 
 /* GET USERNAME BY USERNAME */
 exports.getUsername = async (req, res, next) => {
