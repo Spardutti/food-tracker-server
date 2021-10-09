c0bdb57e1ff39d7690efbacbb2b82bb0a215e0fa
aqwsxzzz <jbarretolarrosa@gmail.com>
2021-10-08 21:26:29 -0300

    added few user functions

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const Recipe = require("../models/Recipe");
 const Ingredient = require("../models/Ingredient");
 const { body, validationResult } = require("express-validator");
+const { findByIdAndUpdate } = require("../models/User");
 
 /* CREATE NEW USER */
 exports.newUser = async (req, res, next) => {
@@ -25,17 +26,14 @@ exports.newUser = async (req, res, next) => {
 
 /* ADD AN EXISTING RECIPE TO THE USER */
 exports.addRecipeUser = async (req, res, next) => {
-  const { id } = req.body;
+  const { recipeID } = req.body;
   try {
-    const recipe = await Recipe.findById(id);
-    let recipeExist = await User.findOne({ recipes: recipe });
-    if (recipeExist) {
-      return res.status(500).json("Recipe already added.");
-    }
+    const recipe = await Recipe.findById(recipeID);
+
     const user = await User.findByIdAndUpdate(
       req.params.id,
       {
-        $push: { recipes: recipe },
+        $addToSet: { recipes: recipe },
       },
       { new: true }
     );
@@ -45,21 +43,97 @@ exports.addRecipeUser = async (req, res, next) => {
     res.json(next(err));
   }
 };
-
 /* REMOVE A RECIPE FROM THE USER FAVORITES */
 exports.deleteRecipeUser = async (req, res, next) => {
-  const { id } = req.body;
+  const { recipeId } = req.body;
   try {
-    const recipe = await Recipe.findById(id);
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: { recipes: recipeId },
+      },
+      { new: true }
+    );
+    res.json(user);
   } catch (err) {
     res.json(next(err));
   }
 };
 
-// DELETE
-
 /* ADD INGREDIENTS TO THE USER FRIDGE */
-// PATCH
+exports.addIngredientFridge = async (req, res, next) => {
+  const { ingredientId, ingredientQty, ingredientName } = req.body;
+  try {
+    const ingredient = await Ingredient.findById(ingredientId);
 
-/* REMOVE INGREDIENTS FROM THE USER FRIDGE */
-// DELETE
+    const ingredientToAdd = {
+      ingredient: ingredientName,
+      quantity: ingredientQty,
+      name: ingredient.name,
+    };
+
+    User.findById(req.params.id, async (err, user) => {
+      if (!user) return res.json("User doesnt exist");
+      const ingredientsArr = user.fridge;
+
+      if (ingredientsArr.length === 0) {
+        ingredientsArr.push(ingredientToAdd);
+      } else {
+        for (let i = 0; i < ingredientsArr.length; i++) {
+          if (ingredientsArr[i].name === ingredient.name) {
+            return res.json(user);
+          }
+        }
+        ingredientsArr.push(ingredientToAdd);
+      }
+      user.markModified("ingredients");
+      await user.save();
+      res.json(user);
+    });
+  } catch (err) {
+    res.json(next(err));
+  }
+};
+
+/* REMOVE INGREDIENT FROM THE USER FRIDGE */
+exports.removeIngredientFridge = async (req, res, next) => {
+  const { ingredientName } = req.body;
+  try {
+    User.findById(req.params.id, async (err, user) => {
+      const ingredientsArr = user.fridge;
+
+      for (let i = 0; i < ingredientsArr.length; i++) {
+        if (ingredientsArr[i].name === ingredientName) {
+          ingredientsArr.splice(i, 1);
+          console.log(ingredientsArr);
+        }
+      }
+      user.markModified("ingredients");
+      await user.save();
+      res.json(user);
+    });
+  } catch (err) {
+    res.json(next(err));
+  }
+};
+
+/* MODIFY INGREDIENTS FRIDGE COUNT */
+exports.modifyIngredientCount = async (req, res, next) => {
+  const { ingredientName, ingredientCount } = req.body;
+  try {
+    User.findById(req.params.id, async (err, user) => {
+      const ingredientsArr = user.fridge;
+
+      for (let i = 0; i < ingredientsArr.length; i++) {
+        if (ingredientsArr[i].name === ingredientName) {
+          ingredientsArr[i].quantity += parseInt(ingredientCount);
+        }
+      }
+      user.markModified("ingredients");
+      await user.save();
+      res.json(user);
+    });
+  } catch (err) {
+    res.json(next(err));
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,18 @@ const userController = require("../Controllers/userController");
 router.post("/new", userController.newUser);
 
 /* ADD RECIPE USER */
-router.patch("/recipe/:id", userController.addRecipeUser);
+router.patch("/recipeadd/:id", userController.addRecipeUser);
+
+/* REMOVE RECIPE USER */
+router.patch("/recipedel/:id", userController.deleteRecipeUser);
+
+/* ADD INGREDIENT FRIDGE */
+router.patch("/ingredientadd/:id", userController.addIngredientFridge);
+
+/* DELETE INGREDIENT FRIDGE */
+router.patch("/ingredientdel/:id", userController.removeIngredientFridge);
+
+/* MODIFY INGREDIENT FRIDGE COUNT */
+router.patch("/ingredientCountMod/:id", userController.modifyIngredientCount);
 
 module.exports = router;
